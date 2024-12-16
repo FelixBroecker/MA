01c7a0969cdc6a6b0b7acc47d97ad640d723d13b
FelixBroecker <felixbroecker90@gmail.com>
2024-12-16 14:49:27 +0100

    Remove plot of CI coefficients from sCI class.

--- sCI/csf.py
+++ sCI/csf.py
@@ -1019,8 +1019,6 @@ class SelectedCI:
             print(
                 f"no file {filename_discarded_all}.wf . Thus it is going to be generated for this selection."
             )
-            # no_file_for_discarded = True
-
         (
             csf_coefficients_optimized,
             csfs_optimized,
@@ -1064,14 +1062,10 @@ class SelectedCI:
                 csf_coefficients_selected,
                 csfs_selected,
                 CI_coefficients_selected,
-                _,
-                _,
-                _,
-            ) = self.cut_csfs(
+            ) = self.sort_csfs_by_CI_coeff(
                 csf_coefficients_optimized,
                 csfs_optimized,
                 CI_coefficients_optimized,
-                0.0,
             )
             (
                 csf_coefficients_discarded,
@@ -1156,73 +1150,6 @@ number of csfs in next iteration wf:\t{len(csf_coefficients[:split_at])}
 number of csfs in residual wf:\t{len(csf_coefficients[split_at:])}
  """
             )
-
-    def plot_ci_coefficients(self, wavefunction_name, n_elec):
-        """plot ci coefficients of wavefunction"""
-        import matplotlib.pyplot as plt
-
-        csf_coefficients, csfs, CI_coefficients, _ = self.read_AMOLQC_csfs(
-            f"{wavefunction_name}.wf", n_elec
-        )
-        # sort CI coefficients from largest to smallest absolut value and respectively csfs and csf_coefficients
-        CI_coefficients_abs = -np.abs(np.array(CI_coefficients))
-        idx = CI_coefficients_abs.argsort()
-
-        CI_coefficients = [abs(CI_coefficients[i]) for i in idx]
-        csf_coefficients = [csf_coefficients[i] for i in idx]
-        csfs = [csfs[i] for i in idx]
-
-        # plot ci coefficients
-        plt.rcParams["figure.figsize"] = (10, 6.5)
-        plt.rcParams["font.size"] = 16
-        plt.rcParams["lines.linewidth"] = 3
-        # plt.rcParams['font.family'] = 'Avenir'
-        plt.rcParams["mathtext.fontset"] = "dejavuserif"
-        plt.rc("axes", titlesize=16)
-        plt.rc("axes", labelsize=16)
-        plt.rc("axes", linewidth=2)
-        color = []
-        ref_state = self.build_energy_lowest_detetminant(n_elec)
-        # color ci points by respective excitation
-        for csf in csfs:
-            difference = 0
-            for electron in csf[0]:
-                if electron not in ref_state:
-                    difference += 1
-            if difference == 1:
-                color.append("darkred")
-            elif difference == 2:
-                color.append("grey")
-            elif difference == 3:
-                color.append("teal")
-            elif difference == 4:
-                color.append("orange")
-            elif difference == 5:
-                color.append("blue")
-            elif difference == 6:
-                color.append("lime")
-            else:
-                color.append("fuchsia")
-
-        plt.xlabel("determinant idx [ ]", labelpad=15)
-        plt.ylabel("CI coefficients [ ]", labelpad=15)
-        plt.scatter(
-            range(len(CI_coefficients) - 1),
-            CI_coefficients[1:],
-            s=15,
-            marker=".",
-            color=color[1:],
-            label="CI coefficients",
-        )
-        plt.legend(
-            loc="upper right",
-        )
-        plt.savefig(
-            f"sCI_{wavefunction_name}.png",
-            format="png",
-            bbox_inches="tight",
-            dpi=450,
-        )
 
 
 if __name__ == "__main__":

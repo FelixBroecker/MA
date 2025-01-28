aa1bb4d5d0c2ebdca67a02421f399ef0ce66bdcc
FelixBroecker <felixbroecker90@gmail.com>
2025-01-28 14:12:03 +0100

    Add reduction of a representation as function

--- sCI/charactertables.py
+++ sCI/charactertables.py
@@ -23,9 +23,31 @@ class CharacterTable:
 
     def multiply(self, characters_1, characters_2):
         """elementwise character multiplication of two irreps"""
-        assert len(characters_1) == len(characters_2), "characters are of unequal length"
+        assert len(characters_1) == len(
+            characters_2
+        ), "characters are of unequal length"
         res = [i * j for i, j in zip(characters_1, characters_2)]
         return res
+
+    def get_reduction(self, representation: list):
+        """"""
+        assert len(representation) == len(
+            self.characters
+        ), "Length of representation does not match with length of characters."
+        g = [int(i.split(" ")[0]) for i in self.operations]
+        contributions = []
+        for label, charac in self.characters.items():
+            contribution = int(
+                sum(
+                    [
+                        g[i] * representation[i] * char
+                        for i, char in enumerate(charac)
+                    ]
+                )
+                / self.order
+            )
+            contributions.append(contribution)
+        return contributions, list(self.characters.keys())
 
     def character2label(self, character):
         """translate character to mulliken label of character table"""
@@ -34,27 +56,22 @@ class CharacterTable:
                 return label
         return None
 
-
     def cs(self):
         """load character table d2h"""
         self.operations = [
             "1 E",
             "1 sh",
-            ]
-        self.characters = {
-            "A'" : [1,1],
-            "A''": [1,-1]
-        }
+        ]
+        self.characters = {"A'": [1, 1], "A''": [1, -1]}
         self.linear_funcs = {
-            "A'" : ["x", "y", "Rz"],
+            "A'": ["x", "y", "Rz"],
             "A''": ["z", "Rx", "Ry"],
         }
         self.quadratic_funcs = {
-            "A'" : ["xx","yy","zz", "xy"],
+            "A'": ["xx", "yy", "zz", "xy"],
             "A''": ["yz", "xz"],
         }
         self.order = 2
-
 
     def d2h(self):
         """load character table d2h"""
@@ -63,33 +80,33 @@ class CharacterTable:
             "1 C2_z",
             "1 sv_xz",
             "1 sv_yz",
-            ]
+        ]
         self.characters = {
-            "Ag" : [1,1,1,1,1,1,1,1],
-            "B1g": [1,1,-1,-1,1,1,-1,-1],
-            "B2g": [1,-1,1,-1,1,-1,1,-1],
-            "B3g": [1,-1,-1,1,1,-1,-1,1],
-            "Au" : [1,1,1,1,-1,-1,-1,-1],
-            "B1u": [1,1,-1,-1,-1,-1,1,1],
-            "B2u": [1,-1,1,-1,-1,1,-1,1],
-            "B3u": [1,-1,-1,1,-1,1,1,-1],
+            "Ag": [1, 1, 1, 1, 1, 1, 1, 1],
+            "B1g": [1, 1, -1, -1, 1, 1, -1, -1],
+            "B2g": [1, -1, 1, -1, 1, -1, 1, -1],
+            "B3g": [1, -1, -1, 1, 1, -1, -1, 1],
+            "Au": [1, 1, 1, 1, -1, -1, -1, -1],
+            "B1u": [1, 1, -1, -1, -1, -1, 1, 1],
+            "B2u": [1, -1, 1, -1, -1, 1, -1, 1],
+            "B3u": [1, -1, -1, 1, -1, 1, 1, -1],
         }
         self.linear_funcs = {
-            "Ag" : [],
+            "Ag": [],
             "B1g": ["Rz"],
             "B2g": ["Ry"],
             "B3g": ["Rx"],
-            "Au" : [],
+            "Au": [],
             "B1u": ["z"],
             "B2u": ["y"],
             "B3u": ["x"],
         }
         self.quadratic_funcs = {
-            "Ag" : ["xx","yy","zz"],
+            "Ag": ["xx", "yy", "zz"],
             "B1g": ["xy"],
             "B2g": ["xz"],
             "B3g": ["yz"],
-            "Au" : [],
+            "Au": [],
             "B1u": [],
             "B2u": [],
             "B3u": [],
@@ -102,49 +119,60 @@ class CharacterTable:
             "1 E",
             "2 C4_z",
             "1 C2",
-            "2 Cprim2",
-            "2 Cprimprim2",
+            "2 C'2",
+            "2 C''2",
             "1 i",
             "2 S4",
             "1 sh",
             "2 sv",
             "2 sd",
-            ]
+        ]
         self.characters = {
-            "A1g": [1,1,1,1,1,1,1,1,1,1],
-            "A2g": [1,1,1,-1,-1,1,1,1,-1,-1],
-            "B1g": [1,-1,1,1,-1,1,-1,1,1,-1],
-            "B2g": [1,-1,1,-1,1,1,-1,1,-1,1],
-            "Eg" : [2,0,-2,0,0,2,0,-2,0,0],
-            "A1u": [1,1,1,1,1,-1,-1,-1,-1,-1,],
-            "A2u": [1,1,1,-1,-1,-1,-1,-1,1,1],
-            "B1u": [1,-1,1,1,-1,-1,1,-1,-1,1],
-            "B2u": [1,-1,1,-1,1,-1,1,-1,1,-1],
-            "Eu" : [2,0,-2,0,0,-2,0,2,0,0],
+            "A1g": [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            "A2g": [1, 1, 1, -1, -1, 1, 1, 1, -1, -1],
+            "B1g": [1, -1, 1, 1, -1, 1, -1, 1, 1, -1],
+            "B2g": [1, -1, 1, -1, 1, 1, -1, 1, -1, 1],
+            "Eg": [2, 0, -2, 0, 0, 2, 0, -2, 0, 0],
+            "A1u": [
+                1,
+                1,
+                1,
+                1,
+                1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+            ],
+            "A2u": [1, 1, 1, -1, -1, -1, -1, -1, 1, 1],
+            "B1u": [1, -1, 1, 1, -1, -1, 1, -1, -1, 1],
+            "B2u": [1, -1, 1, -1, 1, -1, 1, -1, 1, -1],
+            "Eu": [2, 0, -2, 0, 0, -2, 0, 2, 0, 0],
         }
         self.linear_funcs = {
             "A1g": [],
             "A2g": ["Rz"],
             "B1g": [],
             "B2g": [],
-            "Eg" : ["Rx","Ry"],
+            "Eg": ["Rx", "Ry"],
             "A1u": [],
             "A2u": ["z"],
             "B1u": [],
             "B2u": [],
-            "Eu" : ["x","y"],
+            "Eu": ["x", "y"],
         }
         self.quadratic_funcs = {
             "A1g": ["xx+yy", "zz"],
             "A2g": [],
             "B1g": ["xx-yy"],
             "B2g": ["xy"],
-            "Eg" : ["xz", "yz"],
+            "Eg": ["xz", "yz"],
             "A1u": [],
             "A2u": [],
             "B1u": [],
             "B2u": [],
-            "Eu" : [],
+            "Eu": [],
         }
         self.order = 16
 
@@ -155,35 +183,39 @@ class CharacterTable:
             "1 C2_z",
             "1 sv_xz",
             "1 sv_yz",
-            ]
+        ]
         self.characters = {
-            "A1" : [1,1,1,1],
-            "A2" : [1,1,-1,-1],
-            "B1" : [1,-1,1,-1],
-            "B2" : [1,-1,-1,1],
+            "A1": [1, 1, 1, 1],
+            "A2": [1, 1, -1, -1],
+            "B1": [1, -1, 1, -1],
+            "B2": [1, -1, -1, 1],
         }
         self.linear_funcs = {
-            "A1" : ["z"],
-            "A2" : ["Rz"],
-            "B1" : ["x", "Ry"],
-            "B2" : ["y", "Rx"],
+            "A1": ["z"],
+            "A2": ["Rz"],
+            "B1": ["x", "Ry"],
+            "B2": ["y", "Rx"],
         }
         self.quadratic_funcs = {
-            "A1" : ["xx", "yy", "zz"],
-            "A2" : ["xy"],
-            "B1" : ["xz"],
-            "B2" : ["yz"],
+            "A1": ["xx", "yy", "zz"],
+            "A2": ["xy"],
+            "B1": ["xz"],
+            "B2": ["yz"],
         }
         self.cubic_funcs = {
-            "A1" : ["zzz", "xxz", "yyz"],
-            "A2" : ["xyz"],
-            "B1" : ["xzz", "xxx", "xyy"],
-            "B2" : ["yzz", "yyy", "xxy"],
+            "A1": ["zzz", "xxz", "yyz"],
+            "A2": ["xyz"],
+            "B1": ["xzz", "xxx", "xyy"],
+            "B2": ["yzz", "yyy", "xxy"],
         }
         self.order = 4
+
 
 if __name__ == "__main__":
     symmetry = CharacterTable("c2v")
     print(symmetry.characters)
-    res = symmetry.multiply(symmetry.characters["A1"], symmetry.characters["A2"])
+    res = symmetry.multiply(
+        symmetry.characters["A1"], symmetry.characters["A2"]
+    )
     print(res)
+    print(f"reduction: {symmetry.get_reduction([2, 0, 0, 2])}")

79f7a34953c1ee8c8767e032e6bc5187f0073ddd
FelixBroecker <felixbroecker90@gmail.com>
2025-02-18 08:53:25 +0100

    Add class to couple angular momentum of diatomics

---
+++ sCI/diatomics.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import numpy as np
+from salc import SALC
+from csf import SelectedCI
+
+
+class Diatomic:
+    """
+    couple angular momentum and generate excited determinants
+    for linear diatomic molecules
+    """
+
+    def __init__(self): ...
